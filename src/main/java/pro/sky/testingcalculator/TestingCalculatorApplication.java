79e4daafd0ceb605505beb75955ed9330a6f2f57
letuu <albertvt@ya.ru>
2022-12-10 15:18:52 +0300

    Initial commit

---
+++ src/main/java/pro/sky/testingcalculator/TestingCalculatorApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.testingcalculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingCalculatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestingCalculatorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pro/sky/testingcalculator/TestingCalculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.testingcalculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestingCalculatorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
